79433dc993b815c4748fc7e30ee99da1d6cb520c
Alejandra Garcia Muniz <alejandra@MacBook-Air-de-Alejandra.local>
2015-09-18 11:40:34 +0200

    Initial repo

---
+++ customlogin.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Customlogin
+Version: 1.0
+Description: Customize login page
+Author: Pinto
+Author URI: http://bitado.com
+Plugin URI: http://bitado.com
+Text Domain: customlogin
+Domain Path: /languages
+*/
+
